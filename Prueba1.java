3f46348c5d5846c4e2aa2e26eca2655153eaec3c
Emiliano Sensini <emilianosensini@gmail.com>
2024-09-10 08:23:21 -0300

    Primer commit de forma local, ejecicio 3

---
+++ Prueba1.java
@@ -0,0 +1,16 @@
+package prueba1;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+
+	}
+	private boolean esPar(int i) {
+		boolean toRet=false;
+		if(i%2==0) {
+			toRet=true;
+		}
+		return toRet;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # repo-publico
 repo-publico
+modifico local
